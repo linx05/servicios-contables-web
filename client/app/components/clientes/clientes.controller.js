8a0c31e39a2b160d1d9ca45c8bc9f46e5b69d0fd
Cesar Laredo <cesar.f.laredo@gmail.com>
2016-12-04 22:20:53 -0700

    Clientes fix.

--- client/app/components/clientes/clientes.controller.js
+++ client/app/components/clientes/clientes.controller.js
@@ -3,7 +3,7 @@ class ClientesController {
         this.clientesService = ClientesService;
         this.modal = ModalService;
         this.modalOptions = {
-            component: '<pagos-edit></pagos-edit>',
+            component: '<clientes-edit></clientes-edit>',
             title: 'Cliente',
         };
     }
